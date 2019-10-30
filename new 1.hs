531dc78b5e935ff99b03fb9e5168a48c51241763
KorchaginDanil19214 <55308321+KorchaginDanil19214@users.noreply.github.com>
2019-10-30 15:23:37 +0700

    Update new 1.hs

--- new 1.hs
+++ new 1.hs
@@ -6,12 +6,12 @@ toDecimal base snumber | base == 1 = show (length snumber)
 		               | base > 1 && base < 62 = count base snumber
      	 	           | otherwise = error "the number system is incorrect"
 		        where
-				count base snumber = show (foldl (\y number -> if (numInNum(ord number) < base) then y * base + numInNum( ord number) else error "num out range") 0 snumber)
 			     numInNum :: Int->Int
 			     numInNum x |  x > 96 &&  x < 123 = x - 87
 				            |  x > 47 &&  x < 58 =  x - 48
 		      	  	        |  x > 64 &&  x < 91 =  x - 29
 			                | otherwise = error "incorecct symbol"
+							count base snumber = show (foldl (\y number -> if (numInNum(ord number) < base) then y * base + numInNum( ord number) else error "num out range") 0 snumber)
 						   
 			     
 fromDecimal :: Int -> String -> String
@@ -21,11 +21,11 @@ fromDecimal base snumber  | base == 1 = replicate (((read (snumber)::Int)+1) '1'
 			   where
 				count :: Int -> Int -> String -> String
 				count base 0 num = num
-				count base snumber num = count base (div snumber base) ((chr(intToChar (mod snumber base))):num)
+				count base snumber num = count base (div snumber base) ((chr (intToChar (mod snumber base)) ):num)
 				 where					
 				    intToChar x | x >= 0 && x < 10 = x + 48
-				       		    | x >=10 && x < 36 = x + 87
-						        | x >= 36 && x < 62 = x + 29
+				       		    | x >9 && x < 36 = x + 87
+						        | x > 35 && x < 62 = x + 29
 	   					        | otherwise = error "incorecct number"
 						   												
 convertFromTo :: Int -> Int -> String -> String
